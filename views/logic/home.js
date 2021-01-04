7dec9740e9da86e985651ee7440480f2dcdf3b03
Aman Kumar <2019ugcs003@nitjsr.ac.in>
2021-01-04 15:03:42 +0530

    home fixes

--- views/logic/home.js
+++ views/logic/home.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHeiSqmtWHA5qMKPyU6dNR0wXuAmxvfME",
+    authDomain: "app2pcon2k20.firebaseapp.com",
+    databaseURL: "https://app2pcon2k20.firebaseio.com",
+    projectId: "app2pcon2k20",
+    storageBucket: "app2pcon2k20.appspot.com",
+    messagingSenderId: "502599994077",
+    appId: "1:502599994077:web:6c985234b8b2ddf8d66934"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
 var uid =sessionStorage.getItem('manageHrSessionUID');
 // console.log(uid);
 if(uid == null){
@@ -13,3 +26,16 @@ $('#exampleModal').on('show.bs.modal', event => {
 $('#sign_up_btn').click(()=>{
     location.href='signup';
 });
+
+function signIn(){
+
+    firebase.auth().signInWithEmailAndPassword()
+  .then(({user}) => {
+      var id = user.uid;
+    sessionStorage.setItem('manageHrSessionUID',`${id}`);
+    location.href = 'home';
+  })
+  .catch((error) => {
+    location.href = 'error';
+  });
+}
